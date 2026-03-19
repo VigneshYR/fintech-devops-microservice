48ad189937f30a21c7e7150ede7beb70d09153c3
Vignesh YR <vigneshprasadvin@gmail.com>
2026-03-19 07:37:37 +0530

    Update test_payment.py

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -2,7 +2,7 @@ import requests
 
 def test_payment_success():
     response = requests.post(
-        "http://localhost:5000/process-payment",
+        "https://54.227.176.119/process-payment",
         json={"card": "1234567812345678", "amount": 100}
     )
     assert response.status_code == 200
